a51c2b5f465ab4f80ab1948033b10ade1293fd2b
Yuri Marinho <ymarinho2025@gmail.com>
2026-03-12 15:22:59 -0300

    Solução ruim adicionada a exceções personalizadas

---
+++ POO-Java/05-excecoes-personalizadas/solucao-ruim/model/entities/Reservation.java
@@ -0,0 +1,72 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		super();
+		this.roomNumber = roomNumber;
+		this.checkIn = checkin;
+		this.checkOut = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		 Date now = new Date();
+	        if (checkIn.before(now) || checkOut.before(now)) {
+	        	return "Erro na reserva: a data atualizada da reserva deve ser datas futuras";
+	        }
+	        
+	        if (!checkOut.after(checkIn)) {
+	        	return "Erro na reserva: a data do Check-out deve ser depois da data de Check-in";
+	        }
+	        
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return "Sala "
+				+ roomNumber
+				+", check-in: "
+				+ sdf.format(checkIn)
+				+", check-out: "
+				+ sdf.format(checkOut)
+				+", "
+				+ duration()
+				+" noites";
+				
+	}
+}
