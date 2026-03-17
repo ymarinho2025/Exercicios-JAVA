43e32bf7156ae9a4ed6251fa4f92ded1a704e5fe
Yuri Marinho <ymarinho2025@gmail.com>
2026-03-17 17:19:23 -0300

    Exemplo sem interface

---
+++ POO-Java/07-interfaces/no-interface/model/entities/RentalService.java
@@ -0,0 +1,38 @@
+package model.entities;
+
+import java.time.Duration;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+
+        double basicPayment;
+
+        if (hours <= 12) {
+            basicPayment = pricePerHour * Math.ceil(hours);
+        }
+        else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
+
+
